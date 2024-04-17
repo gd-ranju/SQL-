e75ff7d8c0201939956522c462c0042febf292fe
niranjan <gdniranjangd@gmail.com>
2024-04-16 20:50:09 -0700

    Update 02118-build-the-equation.sql

--- Hard/02118-build-the-equation.sql
+++ Hard/02118-build-the-equation.sql
@@ -19,3 +19,30 @@ from
     order by 2 desc) t
 
 -- no companies listed
+
+
+
+
+
+# Write your MySQL query statement below
+WITH
+    T AS (
+        SELECT
+            power,
+            CASE power
+                WHEN 0 THEN IF(factor > 0, CONCAT('+', factor), factor)
+                WHEN 1 THEN CONCAT(
+                    IF(factor > 0, CONCAT('+', factor), factor),
+                    'X'
+                )
+                ELSE CONCAT(
+                    IF(factor > 0, CONCAT('+', factor), factor),
+                    'X^',
+                    power
+                )
+            END AS it
+        FROM Terms
+    )
+SELECT
+    CONCAT(GROUP_CONCAT(it ORDER BY power DESC SEPARATOR ""), '=0') AS equation
+FROM T;

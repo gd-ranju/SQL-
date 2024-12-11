7a1be8a9097a9939a262ec4048746d6f89a7f5f4
Niranjan <gdniranjangd@gmail.com>
2024-12-10 22:30:19 -0600

    Delete Medium directory

--- Medium/00534-game-play-analysis-iii.sql
+++
@@ -1,8 +0,0 @@
--- running total of games played
--- sum() over()
-
-select player_id, event_date, 
-    sum(games_played) over(partition by player_id order by event_date) as games_played_so_far
-from Activity
-
--- gsn games
